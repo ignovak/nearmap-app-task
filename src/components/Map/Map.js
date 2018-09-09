10d33e2d2b8efa35fa38e83ad086a6188d8a8470
Igor Novak <bezengi@gmail.com>
2018-09-10 00:07:35 +0200

    Add map markers

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './Map.css';
+import backgroundMap from './background-map.jpg';
 
 class Map extends Component {
-    render() {
-        return (
-            <div className="Map">
+  render() {
+    console.log(this.props.data);
+    return (
+      <div className="Map">
+        <img src={backgroundMap} className="Map-background" />
+        {
+          this.props.data.map(
+            _ => <div className="Map-marker" key={_.name} style={{left: _.position[0], top: _.position[1]}}>
             </div>
-        );
-    }
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default Map;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import mapData from './map-data';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App mapData={mapData} />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
