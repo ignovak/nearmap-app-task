94c6295118920f145403ab415822864eacfd6e6d
Igor Novak <bezengi@gmail.com>
2018-09-10 00:48:29 +0200

    Show map details on marker click

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './Map.css';
 import backgroundMap from './background-map.jpg';
+import marker from './marker.png';
+import selectedMarker from './marker-selected.png';
 
 class Map extends Component {
   constructor() {
     super();
     this.state = {
-      selectedMarker: ''
+      selectedMarker: -1
     }
   }
 
   onClick(e) {
     this.setState({
-      selectedMarker: e.target.dataset.name
+      selectedMarker: Number(e.target.dataset.index)
     });
   }
 
@@ -21,13 +23,28 @@ class Map extends Component {
       <div className="Map" onClick={this.onClick.bind(this)}>
         <img src={backgroundMap} className="Map-background" alt="Map background" />
         {
+          this.props.data[this.state.selectedMarker] ?
+            <div className="Map-details">
+              <dl>
+                {
+                  Object.entries(this.props.data[this.state.selectedMarker]).map(([key, value]) => [
+                    <dt className="Map-details-key" key={key}>{key}</dt>,
+                    <dd className="Map-details-value" key={value}>{value}</dd>
+                  ])
+                }
+              </dl>
+            </div> : ''
+        }
+        {
           this.props.data.map(
-            _ => <div
-              className={'Map-marker' + (this.state.selectedMarker === _.name ? ' selected' : '')}
-              key={_.name}
+            (_, index) => <img
+              className="Map-marker"
+              src={this.state.selectedMarker === index ? selectedMarker : marker}
+              alt={_.name}
+              key={index}
               style={{left: _.position[0], top: _.position[1]}}
-              data-name={_.name}
-            ></div>
+              data-index={index}
+            />
           )
         }
       </div>
